366b993c358b9480508d5bea276689548f5b2413
K. R. <rawsonkara@gmail.com>
2024-04-11 16:55:44 -0400

    handle agent actions

--- Assets/Scripts/Agents/WormSimpleAgent.cs
+++ Assets/Scripts/Agents/WormSimpleAgent.cs
@@ -10,6 +10,8 @@ namespace DialogosEngine
     {
         CommandLogger Logger;
         bool _IsInitialized = false;
+        string _ExpectedString = "echo hello <eos>";
+        string _CachedString;
 
         public override void Initialize()
         {
@@ -25,23 +27,56 @@ namespace DialogosEngine
 
         public override void CollectObservations(VectorSensor sensor)
         {
-            var obs = Random.value;
-            sensor.AddObservation(obs);
-            Logger.Log($"[{StepCount}] CollectObservations: {obs}");
+            string simulatedTerminalOutput = "echo hello <eos>";
+            float[] vectorizedOutput = Lexer.VectorizeUTF8(simulatedTerminalOutput);
+
+            System.Array.Resize(ref vectorizedOutput, Lexer.k_MaxBufferLength);
+
+            // Debug
+            //System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            for (int i = 0; i < Lexer.k_MaxBufferLength; i++)
+            {
+                float observationValue = i < vectorizedOutput.Length ? vectorizedOutput[i] : 0f;
+                sensor.AddObservation(observationValue);
+
+                // Debug
+                //sb.Append(observationValue);
+                //sb.Append(i < Lexer.k_MaxBufferLength - 1 ? ", " : "");
+            }
+
+            // Debug
+            //Logger.Log($"[{StepCount}] CollectObservations: " + sb.ToString());
         }
 
-        public override void OnActionReceived(ActionBuffers actionBuffers)
+        public override void OnActionReceived(ActionBuffers actions)
         {
+            float[] _actionArray = actions.ContinuousActions.Array;
+            float _lengthControlValue = _actionArray[0];
 
-            var i = -1;
-            var continuousActions = actionBuffers.ContinuousActions;
-            var output = continuousActions[++i];
-            Logger.Log($"[{StepCount}] OnActionReceived: {output}");
+            Logger.Log($"[{StepCount}] Length Control Value: {_lengthControlValue}");
+
+            int outputLength = Transformer.RoundMax(ref _lengthControlValue);
+
+            Logger.Log($"[{StepCount}] Rounded Output Length: {outputLength}");
+            
+            for (int i = 1; i < _actionArray.Length; i++)
+            {
+                _actionArray[i] = Transformer.Transform(ref _actionArray[i]);
+            }
+
+            AgentUtils.ProcessActionArray(ref _actionArray, outputLength);
+
+            //Logger.Log($"[{StepCount}] Processed Action Array: {string.Join(", ", _actionArray)}");
+
+            _CachedString = Lexer.QuantizeUTF8(_actionArray);
+
+            Logger.Log($"[{StepCount}] Quantized String: {_CachedString}");
         }
 
         void FixedUpdate()
         {
-            if(!_IsInitialized)
+            if (!_IsInitialized)
             {
                 return;
             }
